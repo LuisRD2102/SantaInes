f249350cf48b9d4832cbfb8aa4eb9b2001515e59
LuisRD2102 <luisromerod21@gmail.com>
2022-12-25 10:58:47 -0400

    Fix Registro

--- SantaInesWEB/Models/UsuarioModel.cs
+++ SantaInesWEB/Models/UsuarioModel.cs
@@ -5,16 +5,36 @@ namespace SantaInesWEB.Models
 	public class UsuarioModel
 	{
         [Required(ErrorMessage = "Introduzca un usuario")]
-        [StringLength(30, MinimumLength = 5, ErrorMessage = "El nombre debe de tener entre 5 a 30 caracteres")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El usuario debe de tener entre 5 a 30 caracteres")]
         public string username { get; set; }
-        public string password { get; set; }
-        public int cedula { get; set; }
-        public string nombre_Completo { get; set; }
-        public string apellido_Completo { get; set; }
-        public DateTime fecha_Nacimiento { get; set; }
+
+		[Required(ErrorMessage = "Introduzca contraseña")]
+		public string password { get; set; }
+
+		[Required(ErrorMessage = "Repita la contraseña")]
+		[Compare("password", ErrorMessage = "Las contraseñas no coinciden")]
+		public string password2 { get; set; }
+
+		[Required(ErrorMessage = "Introduzca su cédula")]
+		[StringLength(8, MinimumLength = 8, ErrorMessage = "La cédula debe contener 8 dígitos")]
+		public int cedula { get; set; }
+
+		[Required(ErrorMessage = "Introduzca sus nombre(s)")]
+		public string nombre_Completo { get; set; }
+
+		[Required(ErrorMessage = "Introduzca sus apellido(s)")]
+		public string apellido_Completo { get; set; }
+
+		[Required(ErrorMessage = "Introduzca su fecha de nacimiento")]
+		public DateTime fecha_Nacimiento { get; set; }
         public string sexo { get; set; }
-        public string telefono { get; set; }
-        public string email { get; set; }
+
+		[Required(ErrorMessage = "Introduzca teléfono")]
+		public string telefono { get; set; }
+
+		[Required(ErrorMessage = "Introduzca un correo")]
+		[EmailAddress(ErrorMessage = "Introduzca un correo válido")]
+		public string email { get; set; }
         public Guid id_direccion { get; set; }
     }
 }
