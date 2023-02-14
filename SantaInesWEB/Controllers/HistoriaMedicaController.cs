78ebbf27161da43d1fd7dd5c9f3d115bd57c8aa6
santiagovm2000 <vieirasantiagovieira@gmail.com>
2023-02-13 19:14:55 -0500

    Cambios iniciales HM

---
+++ SantaInesWEB/Controllers/HistoriaMedicaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SantaInesWEB.Models;
+using SantaInesWEB.Servicios.ServicioDireccion;
+using SantaInesWEB.Servicios.ServicioUsuario;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SantaInesWEB.Controllers
+{
+    public class HistoriaMedicaController : Controller
+    {
+        //private readonly IServicioCita _servicioCita;
+
+        //public HistoriaMedicaController(IServicioCita servicioCita)
+        //{
+        //    _servicioCita = servicioCita;
+
+        //}
+        public IActionResult HistoriaMedica()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException!;
+            }
+        }
+
+    }
+}
